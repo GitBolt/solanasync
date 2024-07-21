675a19e00f1663bf47932ba3712100ca44383e23
GitBolt <saabisakhtar@gmail.com>
2024-07-21 18:03:30 +0530

    add: spinner load

--- app/src/pages/workshop/[id]/register.tsx
+++ app/src/pages/workshop/[id]/register.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Flex, Heading, Text, Icon, Center, Button, FormControl, FormLabel, Input, Divider, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Icon, Center, Button, FormControl, FormLabel, Input, Divider, useMediaQuery, Spinner } from '@chakra-ui/react';
 import { FaArrowAltCircleRight, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { Navbar } from '@/components/Navbar';
 import { useRouter } from 'next/router';
@@ -109,93 +109,96 @@ const RegisterPage = () => {
   return (
     <>
       <Navbar />
-      <Flex align="center" justify="center" alignItems="center" justifyContent="center" mt={check600 ? "1rem" : "4rem"}>
-        <Flex direction={check1240 ? 'column' : 'row'} w="1200px" alignItems="center" justifyContent="center" gap="1rem">
+      {!workshop || !workshop.location ?
+        <Center>
+          <Spinner  width="10rem" height="10rem" color="blue.200" thickness='0.3rem'/>
+          </Center> : <Flex align="center" justify="center" alignItems="center" justifyContent="center" mt={check600 ? "1rem" : "4rem"}>
+          <Flex direction={check1240 ? 'column' : 'row'} w="1200px" alignItems="center" justifyContent="center" gap="1rem">
 
-          <Box w={check1240 ? "90%" : "50%"} padding={check600 ? "1rem" : "3rem"} mb={4} bg="linear-gradient(180deg, #14141A 0%, #18181E 100%)" boxShadow="0px 3.59px 31.9px 0px rgba(0, 0, 0, 0.50)" borderRadius="1rem" border="1px solid #1C1C27">
-            <Text fontWeight="700" w="100%" textAlign={check600 ? "center" : "start"} fontSize={check600 ? "2.5rem" : "3.2rem"} color="white">
-              {workshop.name}
-            </Text>
-
-            <Text textAlign={check600 ? "center" : "start"} mt="1rem" color="gray.400" fontSize={check600 ? "1.5rem" : "2rem"}>{workshop.description}</Text>
-            <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={5}>
-              Starts on: <span style={{ color: 'white' }}>{formatDate(workshop.start)} {' '} {formatTime(workshop.start)}</span>
-            </Text>
-
-            <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={4}>
-              Ends on: <span style={{ color: 'white' }}>{formatDate(workshop.end)} {' '} {formatTime(workshop.end)}</span>
-            </Text>
-
-            <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={2}>
-              Location: <span style={{ color: 'white' }}>{workshop.location}</span>
-            </Text>
-
-            <Divider bg="#818599" flex="1" mt="2rem" mb="0.8rem" border="1px solid #2f314f" />
-            <Text fontSize={check600 ? "1.4rem" : "1.rem"} color="#818599">
-              Hosted By
-            </Text>
-
-            <Flex align="center" justify="space-between" mt="0.2rem">
-              <Text fontSize="1.4rem" color="white" >
-                {host.name}
+            <Box w={check1240 ? "90%" : "50%"} padding={check600 ? "1rem" : "3rem"} mb={4} bg="linear-gradient(180deg, #14141A 0%, #18181E 100%)" boxShadow="0px 3.59px 31.9px 0px rgba(0, 0, 0, 0.50)" borderRadius="1rem" border="1px solid #1C1C27">
+              <Text fontWeight="700" w="100%" textAlign={check600 ? "center" : "start"} fontSize={check600 ? "2.5rem" : "3.2rem"} color="white">
+                {workshop.name}
               </Text>
-              <Flex>
-                {host.instagram && <Link href={`https://instagram.com/${host.twitter.replace("@", "")}`} target="_blank">
-                  <Icon as={FaInstagram} boxSize={6} color="#818599" />
-                </Link>}
 
-                {host.twitter && <Link href={`https://twitter.com/${host.twitter.replace("@", "")}`} target="_blank">
-                  <Icon as={FaTwitter} boxSize={6} color="#818599" ml={4} />
-                </Link>}
+              <Text textAlign={check600 ? "center" : "start"} mt="1rem" color="gray.400" fontSize={check600 ? "1.5rem" : "2rem"}>{workshop.description}</Text>
+              <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={5}>
+                Starts on: <span style={{ color: 'white' }}>{formatDate(workshop.start)} {' '} {formatTime(workshop.start)}</span>
+              </Text>
 
+              <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={4}>
+                Ends on: <span style={{ color: 'white' }}>{formatDate(workshop.end)} {' '} {formatTime(workshop.end)}</span>
+              </Text>
+
+              <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="#818599" mt={2}>
+                Location: <span style={{ color: 'white' }}>{workshop.location}</span>
+              </Text>
+
+              <Divider bg="#818599" flex="1" mt="2rem" mb="0.8rem" border="1px solid #2f314f" />
+              <Text fontSize={check600 ? "1.4rem" : "1.rem"} color="#818599">
+                Hosted By
+              </Text>
+
+              <Flex align="center" justify="space-between" mt="0.2rem">
+                <Text fontSize="1.4rem" color="white" >
+                  {host.name}
+                </Text>
+                <Flex>
+                  {host.instagram && <Link href={`https://instagram.com/${host.twitter.replace("@", "")}`} target="_blank">
+                    <Icon as={FaInstagram} boxSize={6} color="#818599" />
+                  </Link>}
+
+                  {host.twitter && <Link href={`https://twitter.com/${host.twitter.replace("@", "")}`} target="_blank">
+                    <Icon as={FaTwitter} boxSize={6} color="#818599" ml={4} />
+                  </Link>}
+
+                </Flex>
               </Flex>
-            </Flex>
-            <Divider mt="0.8rem" bg="#818599" flex="1" border="1px solid #2f314f" />
+              <Divider mt="0.8rem" bg="#818599" flex="1" border="1px solid #2f314f" />
 
 
-            <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="white" mt={4}>
-              Registration Count: {workshop?.attendees?.length ? workshop.attendees.length : 0}
-            </Text>
-          </Box>
+              <Text fontSize={check600 ? "1.2rem" : "1.4rem"} color="white" mt={4}>
+                Registration Count: {workshop?.attendees?.length ? workshop.attendees.length : 0}
+              </Text>
+            </Box>
 
 
 
-          {new Date(workshop.end) > new Date() ? !success ? <Box mb="3rem" w={check1240 ? '90%' : "50%"} p={check1240 ? 1 : 12}>
+            {new Date(workshop.end) > new Date() ? !success ? <Box mb="3rem" w={check1240 ? '90%' : "50%"} p={check1240 ? 1 : 12}>
 
 
-            <form onSubmit={handleSubmit} style={{
-              background: "linear-gradient(180deg, #14141A 0%, #18181E 100%)",
-              padding: "1rem 2rem",
-              borderRadius: "1rem",
+              <form onSubmit={handleSubmit} style={{
+                background: "linear-gradient(180deg, #14141A 0%, #18181E 100%)",
+                padding: "1rem 2rem",
+                borderRadius: "1rem",
 
-            }}>
-              <Center>
-                <Heading as="h1" fontSize={check600 ? "2rem" : "3rem"} color="white" mb={5}>
-                  Register
-                </Heading>
-              </Center>
-              <FormControl id="name" mb={4}>
-                <FormLabel color="white">Name</FormLabel>
-                <Input type="text" name="name"
-                  bg="#1F1F29"
-                  borderColor="#34394D"
-                  placeholder='Enter your name'
-                  fontSize="1.5rem"
-                  color="white"
+              }}>
+                <Center>
+                  <Heading as="h1" fontSize={check600 ? "2rem" : "3rem"} color="white" mb={5}>
+                    Register
+                  </Heading>
+                </Center>
+                <FormControl id="name" mb={4}>
+                  <FormLabel color="white">Name</FormLabel>
+                  <Input type="text" name="name"
+                    bg="#1F1F29"
+                    borderColor="#34394D"
+                    placeholder='Enter your name'
+                    fontSize="1.5rem"
+                    color="white"
 
-                  value={formData.name} onChange={handleChange} />
-              </FormControl>
-              <FormControl id="email" mb={4}>
-                <FormLabel color="white">Email</FormLabel>
-                <Input type="email" name="email"
-                  bg="#1F1F29"
-                  borderColor="#34394D"
-                  placeholder='Enter your email'
-                  fontSize="1.5rem"
-                  color="white"
-                  value={formData.email} onChange={handleChange} />
-              </FormControl>
-              {/* <FormControl id="phoneNumber" mb={4}>
+                    value={formData.name} onChange={handleChange} />
+                </FormControl>
+                <FormControl id="email" mb={4}>
+                  <FormLabel color="white">Email</FormLabel>
+                  <Input type="email" name="email"
+                    bg="#1F1F29"
+                    borderColor="#34394D"
+                    placeholder='Enter your email'
+                    fontSize="1.5rem"
+                    color="white"
+                    value={formData.email} onChange={handleChange} />
+                </FormControl>
+                {/* <FormControl id="phoneNumber" mb={4}>
                 <FormLabel color="white">Phone Number</FormLabel>
                 <Input type="text" name="phoneNumber"
                   bg="#1F1F29"
@@ -205,36 +208,36 @@ const RegisterPage = () => {
                   color="white"
                   value={formData.phoneNumber} onChange={handleChange} />
               </FormControl> */}
-              <FormControl id="goals" mb={4}>
-                <FormLabel color="white">Goals</FormLabel>
-                <Input type="text" name="goals"
-                  bg="#1F1F29"
-                  borderColor="#34394D"
-                  placeholder='What is your goal with attending this workshop?'
+                <FormControl id="goals" mb={4}>
+                  <FormLabel color="white">Goals</FormLabel>
+                  <Input type="text" name="goals"
+                    bg="#1F1F29"
+                    borderColor="#34394D"
+                    placeholder='What is your goal with attending this workshop?'
+                    fontSize="1.5rem"
+                    color="white" value={formData.goals} onChange={handleChange} />
+
+                </FormControl>
+                <Button type="submit"
+                  rounded="1rem"
+                  bg="#5F54D8"
+                  mt="2rem"
+                  w="100%"
+                  h="3.5rem"
                   fontSize="1.5rem"
-                  color="white" value={formData.goals} onChange={handleChange} />
+                  rightIcon={<Icon as={FaArrowAltCircleRight} />}
+                  colorScheme="voilet">
+                  Register
+                </Button>
+              </form>
+            </Box> :
+              <Text color="white" textAlign="center" fontSize="3rem" width="50%">You are in! Feel free to close the tab now.</Text>
+              : <Box mb="2rem" w={check1240 ? "80%" : '50%'}>
+                <p style={{ fontSize: "2.5rem", color: '#cacafb', fontWeight: "700", textAlign: 'center' }}>The workshop is over. Thanks for joining!</p>
 
-              </FormControl>
-              <Button type="submit"
-                rounded="1rem"
-                bg="#5F54D8"
-                mt="2rem"
-                w="100%"
-                h="3.5rem"
-                fontSize="1.5rem"
-                rightIcon={<Icon as={FaArrowAltCircleRight} />}
-                colorScheme="voilet">
-                Register
-              </Button>
-            </form>
-          </Box> :
-            <Text color="white" textAlign="center" fontSize="3rem" width="50%">You are in! Feel free to close the tab now.</Text>
-            : <Box mb="2rem" w={check1240 ? "80%" : '50%'}>
-              <p style={{ fontSize: "2.5rem", color: '#cacafb', fontWeight: "700", textAlign: 'center' }}>The workshop is over. Thanks for joining!</p>
-
-            </Box>}
-        </Flex >
-      </Flex >
+              </Box>}
+          </Flex >
+        </Flex >}
 
     </>
   );
